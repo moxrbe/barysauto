1d6c4f5e5513d9734bc24ce36b1e1a9dbd571b19
moxrbe <one393752@gmail.com>
2021-07-21 22:25:50 +0500

    edit burger

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,20 @@
-var x = document.getElementById("contain");
+const mainMenu = document.querySelector('.header__nav-list');
+const openMenu = document.querySelector('.nav__menu');
+const closeMenu = document.querySelector('.nav__close');
 
-x.addEventListener("click", myFunction);
 
-function myFunction() {
-  var element = document.getElementById("nav");
-  element.classList.toggle("open");
-  
-  x.classList.toggle("change");
+
+
+
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+    console.log(show);
+}
+function close(){
+    mainMenu.style.top = '-100%';
+    console.log(close);
 }
