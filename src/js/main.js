f148aa23df6f8f78b81a37ae398e2d92d40b4580
moxrbe <one393752@gmail.com>
2021-07-23 14:14:54 +0500

    edit media

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,12 @@
 const mainMenu = document.querySelector('.header__nav-list');
 const openMenu = document.querySelector('.nav__menu');
-const closeMenu = document.querySelector('.nav__close');
 
 
 
 
 
-openMenu.addEventListener('click',show);
-closeMenu.addEventListener('click',close);
 
-function show(){
-    mainMenu.style.display = 'flex';
-    mainMenu.style.top = '0';
-    console.log(show);
-}
-function close(){
-    mainMenu.style.top = '-100%';
-    console.log(close);
-}
+openMenu.addEventListener('click', () => {
+    mainMenu.classList.toggle('active')
+});
+
